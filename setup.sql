9b6041873ae7d81fd19af23d33f7c0aa59e00593
Sean Morris <sean.morris@snowflake.com>
2025-04-18 09:54:17 -0600

    Update setup.sql

--- setup.sql
+++ setup.sql
@@ -1,15 +1,33 @@
+-- Set role context
 USE ROLE accountadmin;
-USE WAREHOUSE compute_wh;
 
-CREATE DATABASE IF NOT EXISTS AI209_db;
+-- Set Environment Name
+SET var_name = 'ai209';
 
--- Optional: Rename Database
-// ALTER DATABASE multimodal_db RENAME TO AI209_db;
+-- Generate Environment Identifiers
+SET var_warehouse_name = $var_name||'_wh';
+SET var_database_name = $var_name||'_db';
+SET var_schema_name = $var_name||'_db.demo';
 
-USE SCHEMA public;
+-- Enable Cortex Cross Region Inference
+ALTER ACCOUNT SET CORTEX_ENABLED_CROSS_REGION = 'ANY_REGION';
+
+-- Set Warehouse Context
+CREATE WAREHOUSE IF NOT EXISTS IDENTIFIER($var_warehouse_name) WITH
+    WAREHOUSE_SIZE = xsmall
+    AUTO_SUSPEND = 60
+    INITIALLY_SUSPENDED = true;
+
+USE WAREHOUSE IDENTIFIER($var_warehouse_name);
+
+-- Set Database Schema Context
+CREATE DATABASE IF NOT EXISTS IDENTIFIER($var_database_name);
+
+CREATE SCHEMA IF NOT EXISTS IDENTIFIER($var_schema_name);
+
+USE SCHEMA IDENTIFIER($var_schema_name);
     
--- Create an internal stage
-CREATE OR REPLACE STAGE images_stage
+-- Create Images Stage
+CREATE STAGE IF NOT EXISTS images_stage
     DIRECTORY = ( ENABLE = true )
-    ENCRYPTION = ( TYPE = 'SNOWFLAKE_SSE' )
-;
+    ENCRYPTION = ( TYPE = 'SNOWFLAKE_SSE' );
